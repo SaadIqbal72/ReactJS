2faf85bfbb6b47dab36f8945a61de779ed6b0048
Mudassir <mudasir@fyxify.com>
2026-05-21 00:59:47 +0500

    Working

--- src/components/bid_card.jsx
+++ src/components/bid_card.jsx
@@ -3,12 +3,10 @@ import nftImage from "../assets/images/nft-1.svg"
 
 const BidCard = () => {
     return (
-
         <div className="p-[15px] flex flex-col gap-[15px] rounded-[24px] bg-[#2F3642]">
-            <div className="rounded-[15px] overflow-hidden h-[300px] border-[2px] border-[#444]">
+            <div className="rounded-[15px] overflow-hidden h-[300px] border-[2px] border-[#454C57]">
                 <img src={nftImage} alt="NFT Image" className="w-full h-full" />
             </div>
-
             <h6 className="m-0 font-poppins font-medium text-[22px] text-[#fff]">Inner portrait #3425</h6>
             <div className="flex items-center justify-between">
                 <h6 className="m-0 font-poppins text-[18px] text-[#fff]">
@@ -17,7 +15,7 @@ const BidCard = () => {
                 </h6>
                 <div className="flex flex-col">
                     <div className="flex items-center gap-[5px]">
-                        <img src={ethreumIcon} alt="" className="w-[28px] h-[28px] object-cover" />
+                        <img src={ethreumIcon} alt="EthreumI Icon" className="w-[28px] h-[28px] object-cover" />
                         <p className="m-0 font-poppins font-medium text-[16px] text-[#fff]">15.25</p>
                     </div>
                     <h6 className="m-0 font-poppins font-medium text-[14px] text-[#8F939A]">Highest Bid</h6>
@@ -28,7 +26,6 @@ const BidCard = () => {
                 <div className="w-[40%] h-[48px] flex items-center justify-center rounded-[12px] text-[20px] font-poppins font-medium border text-[#E897B4] border-[#E897B4]">10 : 03 : 12</div>
             </div>
         </div>
-
     )
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,12 +2,14 @@ import BidCard from '../components/bid_card';
 
 const Home = () => {
     return (
-        <div className='max-w-7xl mx-auto'>
-            <h1>Home</h1>
-            <div className='flex flex-col gap-[20px]'>
-                <h1 className='m-0 text-center font-poppins font-bold text-[#fff]'>Trending Actions</h1>
-                <div className="grid grid-cols-3 gap-[15px]">
-                    <BidCard />
+        <div className='min-h-screen bg-[#202835]'>
+            <div className='max-w-7xl mx-auto'>
+                <h1>Home</h1>
+                <div className='flex flex-col gap-[30px]'>
+                    <h1 className='m-0 text-center font-poppins font-bold text-[40px] text-[#E7F1D7]'>Trending Actions</h1>
+                    <div className="grid grid-cols-3 gap-[15px]">
+                        <BidCard />
+                    </div>
                 </div>
             </div>
         </div>
